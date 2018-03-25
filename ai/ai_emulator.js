0f8d856b503d137eff9ea6974a9e8084b46df78b
sara-h-chen <deutscher71112@gmail.com>
2018-03-25 15:24:33 +0100

    Complete wandering AI

--- ai/ai_emulator.js
+++ ai/ai_emulator.js
@@ -1,5 +1,7 @@
 createBotClient = require('./bot_client.js').createBotClient;
 
+var questWaypoints = [{"x":100,"y":9},{"x":18,"y":67},{"x":12,"y":95},{"x":20,"y":119},{"x":76,"y":138},{"x":66,"y":160},{"x":19,"y":209},{"x":63,"y":204},{"x":75,"y":223},{"x":12,"y":237},{"x":76,"y":294},{"x":73,"y":268}];
+
 // TODO: Change this so it doesn't require the default exports to be added in these files
 var EasyStar = require('./easystar.min.js').default;
 
@@ -16,6 +18,10 @@ function sleep(time) {
 
 function randomInt(low, high) {
     return Math.floor(Math.random() * (high - low) + low);
+}
+
+function checkSameCoordinates(player, waypoint) {
+    return ((player.x == waypoint.x) && (player.y == waypoint.y))
 }
 
 
@@ -37,14 +43,54 @@ function doBotStuff(player, index, cycle = 0) {
     setTimeout(() => doBotStuff({x: player.x + delta.x, y: player.y + delta.y}, index, (cycle + 1) % 4), 1000);
 }
 
-function wander(player, index, cycle=0) {
+function wander(player, index) {
+    let start = {x: player.x, y: player.y, queue: player.queue};
+    let timeOfNextChoice = player.timeOfNextChoice;
+    let chosenRandomWaypoint;
 
+    let queue = [];
+    if (player.queue) {
+        queue = player.queue;
+        // takes the last thing in the queue
+        chosenRandomWaypoint = player.waypoint;
+    }
+
+    if (!chosenRandomWaypoint || timeOfNextChoice > Date.now() || checkSameCoordinates(player, chosenRandomWaypoint)) {
+        chosenRandomWaypoint = questWaypoints[Math.floor(Math.random() * questWaypoints.length)];
+        timeOfNextChoice = Date.now() + Math.random() * 10000;
+    }
+
+    let lastPathCell = {};
+
+    pathfindingCallback = (path) => {
+        if (queue.length > 0) {
+            let dequeued = queue.shift();
+            BotClients[index].sendPath(dequeued, {action: 0}, 0);
+            lastPathCell = dequeued[dequeued.length - 1];
+        } else if (path && path.length < 40) {
+            BotClients[index].sendPath(path, {action: 0}, 0);
+            lastPathCell = path[path.length - 1];
+            // if path to quest waypoint is too long, break into chunks
+        } else {
+            let i,j, chunk = 20;
+            for (i = 0, j = path.length; i < j; i += chunk) {
+                queue.push(path.slice(i, i + chunk));
+            }
+            let dequeued = queue.shift();
+            BotClients[index].sendPath(dequeued, {action: 0}, 0);
+            lastPathCell = dequeued[dequeued.length - 1];
+        }
+    };
+    easystar.findPath(start.x, start.y, chosenRandomWaypoint.x, chosenRandomWaypoint.y, pathfindingCallback);
+    easystar.calculate();
+    setTimeout(() => wander({x:lastPathCell.x, y: lastPathCell.y, timeOfNextChoice: timeOfNextChoice, queue: queue, waypoint: chosenRandomWaypoint}, index), 3000);
 }
 
-totalBots = 50;
+totalBots = 1;
 var BotClients = [];
 for (var i = 0; i < totalBots; i++) {
-    BotClients.push(createBotClient(i, doBotStuff));
+    // BotClients.push(createBotClient(i, doBotStuff));
+    BotClients.push(createBotClient(i, wander));
     BotClients[i].requestData();
 }
 
